562f976a913846c9f5a0a9a796526f8de0ab9368
Mr.Meeseeks <emil485h@gmail.com>
2021-12-08 20:52:00 +0100

    Maybe Done

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 
+
 hamburger.addEventListener('click', () => {
 navUL.classList.toggle('show');
     });
+
+    
+// const Logo = document.querySelectorAll("#Logo path");
+    // for(let i = 0; i<Logo.length; i++) {
+    //     console.log(`Letter ${i} is ${Logo[i].getTotalLength()}`);
+    // }
+
+    
